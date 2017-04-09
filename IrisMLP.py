e34320ad4fbb1578e1c921be08c8d4509381b0a7
Radek <Radeklesniewski13@gmail.com>
2017-04-09 20:08:51 +0200

    Backpropagation algorithm - first empiric test

--- IrisMLP.py
+++ IrisMLP.py
@@ -29,7 +29,7 @@ for i in range(3):
 
 mlp.addLayer(layer2)
 
-for i in range(0,1):
+for i in range(0,2):
     for j in mlp.layers[i].neurons:
         for l in mlp.layers[i + 1].neurons:
             w = random.uniform(0.1, 0.5)
@@ -41,16 +41,35 @@ mlp.layers[0].neurons[1].outputValue = 4.4
 mlp.layers[0].neurons[2].outputValue = 3.4
 mlp.layers[0].neurons[3].outputValue = 4.7
 
+mlp.layers[2].neurons[0].computeDeltaParameter(1)
+mlp.layers[2].neurons[1].computeDeltaParameter(0)
+mlp.layers[2].neurons[2].computeDeltaParameter(0)
+
+for i in mlp.layers:
+    for j in i.neurons:
+        j.computeWeightedSum()
+        j.computeOutputValue()
+
 mlp.layers[1].neurons[0].computeWeightedSum()
 mlp.layers[1].neurons[0].computeOutputValue()
-print(mlp.layers[1].neurons[0].outputValue)
+
+mlp.layers[2].neurons[0].computeDeltaParameter(1)
+mlp.layers[2].neurons[1].computeDeltaParameter(0)
+mlp.layers[2].neurons[2].computeDeltaParameter(0)
+
+for i in range(1, -1, -1):
+    for j in mlp.layers[i].neurons:
+        print(i)
+        #dummy parameter
+        j.computeDeltaParameter(0)
+        j.updateWeight(0.8)
+
+print(mlp.layers[1].neurons[0].inputConnections)
+print(mlp.layers[0].neurons[0].outputConnections)
 
 with open('testing_data.csv', newline='\n') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
         i = 0;
         for input in mlp.getLayers():
-            #input.setOutputValue(row[i])
-            i = i + 1
-
-            # launching bp algorithm for every entry
+            print(row[i])
