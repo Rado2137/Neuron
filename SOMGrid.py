9d2596e0f65abdce782fc22e8f297004ced3b26c
Radoslaw Lesniewski <Radoslaw.Lesniewski@sabre.com>
2017-05-07 22:25:38 +0200

    SOM learning algo

--- SOMGrid.py
+++ SOMGrid.py
@@ -1,20 +1,36 @@
+import math
+
 from SOMNeuron import SOMNeuron
 
 
 class SOMGrid:
 
-    def __init__(self, height, width, startingRadius, narrowing, initLearningRate):
+    def __init__(self, height, width, initLearningRate):
         self.width = width
         self.height = height
-        self.startingRadius = startingRadius
-        self.narrowing = narrowing
+        self.startingRadius = height / 2 if height > width else width / 2
+        self.narrowing = 1000
         self.initLearningRate = initLearningRate
         self.matrix = [[SOMNeuron() for x in range(width)] for y in range(height)]
 
     def chooseWinner(self):
+        #TODO to delete
         winnerCoords = (0, 0)
         for i in range(self.height):
             for j in range(self.width):
                 if self.matrix[i][j].distance < self.matrix[winnerCoords[0]][winnerCoords[1]].distance:
                     winnerCoords = (i, j)
         winnerCoords
+
+    def doLearnStep(self, iteration):
+        radius = self.startingRadius * math.exp(- iteration / self.narrowing)
+        learningRate = self.initLearningRate * math.exp(- iteration / self.narrowing)
+
+        #TODO it needs to be computed only once
+        for i in range(self.height):
+            for j in range(self.width):
+                self.matrix[i][j].computeDistance()
+
+        for i in range(self.height):
+            for j in range(self.width):
+                self.matrix[i][j].updateWeight(learningRate, radius)
--- SOMNeuron.py
+++ SOMNeuron.py
@@ -13,3 +13,8 @@ class SOMNeuron(Neuron):
         for k, v in self.inputConnections.items():
             self.distance += math.pow(k.outputValue - v, 2)
         self.distance = math.sqrt(self.distance)
+
+    def updateWeight(self, learningRate, radius):
+        for k, v in self.inputConnections.items():
+            theta = math.exp(-math.pow(self.distance, 2) / (2 * math.pow(radius, 2)))
+            self.inputConnections[k] = v - theta * learningRate * (k.outputValue - v)
